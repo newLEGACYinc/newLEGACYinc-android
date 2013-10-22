cdf89a74591a3b1f204ab3e5c09d620c9122d3c6
Scott Walters <srwalte2@illinois.edu>
2013-10-22 00:18:27 -0500

    Youtube link works
    Clicking youtube button now opens the newLegacyInc youtube channel on
    the Android Youtube app

--- gen/com/scowalt/newlegacyincapp/R.java
+++ gen/com/scowalt/newlegacyincapp/R.java
@@ -33,11 +33,11 @@ public final class R {
         public static final int all=0x7f080000;
         public static final int imageView1=0x7f080006;
         public static final int imageView2=0x7f080007;
-        public static final int imageView3=0x7f080003;
         public static final int imageView4=0x7f080004;
         public static final int social_media_1=0x7f080002;
         public static final int social_media_2=0x7f080005;
         public static final int textView1=0x7f080001;
+        public static final int youtube=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/scowalt/newlegacyincapp/MainActivity.java
+++ src/com/scowalt/newlegacyincapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.scowalt.newlegacyincapp;
 
+import com.google.android.youtube.player.YouTubeIntents;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +17,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Context c = this;
+		ImageView youtube = (ImageView) findViewById(R.id.youtube);
+		youtube.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent intent = YouTubeIntents.createUserIntent(c, "newLEGACYinc");
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
